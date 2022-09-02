e8e614582776d2dc6eacea7137ca977fe0b39938
Chs <1984928137@qq.com>
2022-09-02 23:59:16 +0800

    main

--- src/TS/teacherList.ts
+++ src/TS/teacherList.ts
@@ -16,19 +16,42 @@ interface QuireData {
     boarding?: string,
 }
 
-interface StudentList {
+enum FamilyS {
+    name = 0,
+    studentID = 1,
+    sex = 2,
+    phone = 3,
+    nationality = 4,
+    boarding = 5,
+    IDcard = 6,
+    accountType = 7,
+    censusRegister = 8,
+    currentAddress = 9,
+    whetherChildren = 10,
+}
+
+interface TchOptions {
+    label: string,
+    value: string
+}
+
+interface TeacherList {
     // 出生时间
     time: string,
     name: string,
     id: number | string,
-    // 学号
-    studentID: number | string,
+    // 教师编号
+    TeacherID: number | string,
     sex: number | string,
-    phone: number,
-    // 民族
-    nationality: string,
-    // 寄宿
-    boarding: string,
+    phone: number|string,
+    // 身份证号
+    IDcard:string|number
+    // 地址
+    place: string,
+    // qq号
+    QQ: string|number,
+    // 微信号
+    WX:string|number
     page: number,
     count: number,
     isShow: boolean
@@ -36,16 +59,32 @@ interface StudentList {
 
 
 class InitData {
-    quireData: Ref<QuireData>
-    studentListData: Ref<StudentList>
+    // 时间搜索
+    defaultTime: Ref<Date[]>
+    timeValue1: Ref<string>
+
+    // 按条件查询
+    FieldValue: Ref<string>
+    options: TchOptions[]
     anyField: Ref<string | number>
+
+    // 
+    quireData: Ref<QuireData>
+    teacherListData: Ref<TeacherList>
+    teacherQuierData: Ref<TeacherList[]>
+    formData: TeacherList
+
     pageIndex: Ref<number>
-    tableData: Ref<StudentList[][]>
-    multipleSelection: Ref<StudentList[]>
+    tableData: Ref<TeacherList[][]>
+    multipleSelection: Ref<TeacherList[]>
     multipleTableRef: Ref
     value: Ref<boolean>
-    currentRow: Ref<StudentList | undefined>
+    currentRow: Ref<TeacherList | undefined>
     data: any
+
+    // 控制弹窗是否弹出
+    dialogFormVisible: Ref<boolean>
+
 
     constructor() {
         this.quireData = ref<QuireData>(
@@ -60,82 +99,175 @@ class InitData {
                 boarding: ''
             }
         )
-        this.studentListData = ref<StudentList>({
+        this.formData =
+        {
             id: '',
             time: '',
             name: '',
-            studentID: '',
+            TeacherID: '',
+            IDcard:'',
             sex: '',
             phone: 0,
-            nationality: '',
-            boarding: '',
+            place: '',
+            QQ: '',
+            WX:'',
             page: 1,
             count: 0,
-            isShow: true
+            isShow: true,
+        }
+        this.teacherListData = ref<TeacherList>({
+            id: '',
+            time: '',
+            name: '',
+            TeacherID: '',
+            IDcard: '',
+            sex: '',
+            phone: 0,
+            place: '',
+            QQ: '',
+            WX: '',
+            page: 1,
+            count: 0,
+            isShow: true,
         })
+        this.teacherQuierData = ref([])
         this.anyField = ref<string | number>('')
-        this.pageIndex = ref<number>(0)
-        this.tableData = ref<StudentList[][]>(
+        this.pageIndex = ref<number>(1)
+        this.tableData = ref(
             []
         )
-        this.multipleSelection = ref<StudentList[]>([])
+        this.multipleSelection = ref<TeacherList[]>([])
         this.multipleTableRef = ref<InstanceType<typeof ElTable>>()
         this.value = ref<boolean>(true)
-        this.currentRow = ref<StudentList | undefined>()
+        this.currentRow = ref<TeacherList | undefined>()
+        this.defaultTime = ref([
+            new Date(2000, 1, 1, 0, 0, 0),
+            new Date(2000, 2, 1, 23, 59, 59),
+        ])
+        this.timeValue1 = ref('')
+        this.FieldValue = ref('')
+        this.options = [
+            { label: '名字', value: '0' },
+            { label: '学号', value: '1' },
+            { label: '性别', value: '2' },
+            { label: '手机号码', value: '3' },
+            { label: '民族', value: '4' },
+            { label: '是否住宿', value: '5' },
+            { label: '身份证', value: '6' },
+            { label: '户口类型', value: '7' },
+            { label: '户籍', value: '8' },
+            { label: '居住城市', value: '9' },
+            { label: '是否留守', value: '10' }
+        ]
+        this.dialogFormVisible = ref(false)
 
     }
 
-    productQuire = () => {
-        // this.productQuireData
-        return exRequest.post({
-            url: RouterAPI.Product,
-            data: this.quireData
-        })
+    // 发送请求
+    getTeacherListData = () => {
+        return exRequest.get({
+            url: RouterAPI.StudentList,
+            data: "",
+        });
     }
 
+    // 发送查询请求
     onQuireClick = async () => {
-        console.log('submit!')
-        if (!this.quireData.value.id && !this.quireData.value.name) {
-            return
+        if (!this.timeValue1.value && !this.anyField.value) {
+            console.log('没有值，查询不了')
+            return null
         }
-        this.data = await this.productQuire()
+        console.log('onQuireClick!')
+        // this.data = await this.productQuire()
         // .then(res => {
-        this.tableData.value = []
-        this.studentListData.count = this.data?.result.length == 'undefined' ? 1
-            : this.data?.result.length
-        this.splitArray(this.data?.result)
+        // this.tableData.value = []
+        // this.studentListData.count = this.data?.result.length == 'undefined' ? 1
+        //     : this.data?.result.length
+        // this.splitArray(this.data?.result)
         //     return res.data.result
         // })
         // .catch(err => {
         //     console.log(err)
         // })
+        let quireDatas: any = []
+        console.log(this.FieldValue.value, FamilyS[Number.parseInt(this.FieldValue.value)])
+        if (this.timeValue1.value[0]) {
+            quireDatas = this.timeValue1.value[1] ? this.teacherQuierData.value.filter(
+                v => Date.parse(this.timeValue1.value[0]) <= Date.parse(v.time) && Date.parse(v.time) <= Date.parse(this.timeValue1.value[1])
 
-        // this.tableData = this.obj.value
-        console.log('onSubmit', this.tableData.value)
-
+            )
+                : this.teacherQuierData.value.filter(
+                    v => Date.parse(this.timeValue1.value[0]) <= Date.parse(v.time)
+                )
+            if (this.anyField.value) {
+                quireDatas = quireDatas.filter(
+                    (v: unknown) => {
+                        // console.log(v)
+                        return this.DataFilters(v, FamilyS[Number.parseInt(this.FieldValue.value)], this.anyField.value)
+                    }
+                )
+            }
+        } else if (this.quireData.value.name) {
+            quireDatas = this.teacherQuierData.value.filter(
+                v => v.name.indexOf(this.quireData.value.name as string) != -1
+            )
+        } else if (this.anyField.value) {
+            quireDatas = this.teacherQuierData.value.filter(v => {
+                // console.log(v)
+                return this.DataFilters(v, FamilyS[Number.parseInt(this.FieldValue.value)], this.anyField.value)
+            })
+        }
+        console.log(Array.isArray(quireDatas))
+        this.tableData.value = []
+        this.splitArray(quireDatas)
+        this.teacherListData.value.count = quireDatas.length
     }
     handleCurrentChange = (val: number) => {
         this.pageIndex.value = val
         console.log('pageIndex', this.pageIndex.value)
-        this.studentListData.value.page = val
+        this.teacherListData.value.page = val
     }
-    splitArray = (val: StudentList[]) => {
+    splitArray = (val: TeacherList[]) => {
         for (let index = 0; index < val.length; index += 5) {
-            let list: StudentList[] = val.slice(index, index + 5)
+            let list: TeacherList[] = val.slice(index, index + 5)
             this.tableData.value.push([...list])
         }
         console.log('splitArray', this.tableData.value)
     }
 
-    handleSelectionChange = (val: StudentList[]) => {
+    DataFilters = (list: any, field: string, key: string | number | undefined) => {
+
+
+        if (Object.prototype.hasOwnProperty.call(list, field)) {
+            console.log(list[field], key, field)
+            const element = list[field];
+            if (element.toString().indexOf(key) >= 0) {
+
+                return list
+            } else {
+                for (const keys in list) {
+                    const element = list[keys];
+                    if (element.toString().indexOf(key) >= 0) {
+                        console.log(list[keys], key, keys)
+                        return list
+                    }
+                }
+            }
+        }
+
+        // console.log("arr",arr)
+        // return arr
+    }
+
+    handleSelectionChange = (val: TeacherList[]) => {
         console.log(val)
         this.multipleSelection.value = val
     }
-    handleCurrentChanges = (val: StudentList | undefined) => {
+    handleCurrentChanges = (val: TeacherList | undefined) => {
         console.log(val, '566')
         this.currentRow.value = val
     }
-    toggleSelection = (rows?: StudentList[]) => {
+    toggleSelection = (rows?: TeacherList[]) => {
         if (rows) {
             rows.forEach((row) => {
 
@@ -145,7 +277,7 @@ class InitData {
             this.multipleTableRef.value!.clearSelection()
         }
     }
-    handleEdit = (index: number, row: StudentList) => {
+    handleEdit = (index: number, row: TeacherList) => {
 
         // row.isShow = true
         // this.[index].value = row.price
@@ -155,8 +287,54 @@ class InitData {
         // row._id == this.currentRow.value._id ? this.rowShow.value = true 
         // : this.rowShow.value = false
     }
-    handleDelete = (index: number, row: StudentList) => {
+    handleDelete = (index: number, row: TeacherList) => {
         console.log(index, row)
+    }
+
+    // 取消查询
+    onRestoreClick = () => {
+        this.tableData.value = []
+        let RestoreData: TeacherList[] = JSON.parse(JSON.stringify(this.teacherQuierData.value))
+        this.splitArray(RestoreData)
+        this.teacherListData.value.count = RestoreData.length
+    }
+
+    // 导出老师信息
+    tableToExcel = async () => {
+        // 要导出的json数据
+
+        const jsonData: Ref = ref();
+        this.tableData.value = []
+        jsonData.value = await this.getTeacherListData();
+        this.teacherQuierData = jsonData.value?.result;
+        // .then((res) => {
+        this.teacherListData.count = jsonData.value?.result.length;
+        this.splitArray(jsonData.value?.result);
+        // 列标题，逗号隔开，每一个逗号就是隔开一个单元格
+        let str = `学号,姓名,电话,邮箱\n`;
+        // 增加\t为了不让表格显示科学计数法或者其他格式
+        for (let i = 0; i < jsonData.value?.result.length; i++) {
+            for (let key in jsonData.value?.result[i]) {
+                if (key == "_id" || key == "permission" || key == "postData") {
+                    continue
+                }
+                if (key == 'familyMember') {
+                    console.log(jsonData.value.result[i][key][0],
+                        Object.prototype.toString.call(jsonData.value.result[i][key]) == "[object Array]")
+                }
+                str += `${jsonData.value.result[i][key] + '\t'},`;
+            }
+            str += '\n';
+        }
+        console.log("str", str)
+        // encodeURIComponent解决中文乱码
+        const uri = 'data:text/csv;charset=utf-8,\ufeff' + encodeURIComponent(str);
+        // 通过创建a标签实现
+        const link = document.createElement("a");
+        link.href = uri;
+        // 对下载的文件命名
+        link.download = "学生信息表.csv";
+        link.click();
     }
 }
 
@@ -164,4 +342,7 @@ class InitData {
 
 export {
     InitData
+}
+export type {
+    TeacherList,
 }
--- src/components/Uncommon/teacherList.vue
+++ src/components/Uncommon/teacherList.vue
@@ -1,77 +1,142 @@
 <template>
+  <div>
+    <!-- 顶部查询框 -->
     <div>
-    <div>
-      <el-form :inline="true" :model="studentListData" class="demo-form-inline">
-        <el-form-item label="学号">
-          <el-input v-model="studentListData.studentID" placeholder="搜索" />
-        </el-form-item>
-        <el-form-item label="名字">
-          <el-input v-model="studentListData.name" placeholder="搜索">
-            <!-- <el-option label="Zone one" value="shanghai" /> -->
-            <!-- <el-option label="Zone two" value="beijing" /> -->
-          </el-input>
-        </el-form-item>
-        <el-form-item label="任意条件查询">
-          <el-input v-model="anyField" placeholder="搜索" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onQuireClick">查询</el-button>
-          <el-button type="primary" @click="toggleSelection()"
-            >Clear selection</el-button
+      <el-form :inline="true" :model="quireData" class="demo-form-inline">
+        <el-form-item label="按时间" class="dateItem">
+          <el-date-picker
+            v-model="timeValue1"
+            type="daterange"
+            :start-placeholder="Times.reTime(defaultTime[0])"
+            :end-placeholder="Times.reTime(defaultTime[1])"
+            :default-time="defaultTime"
+            format="YYYY/MM/DD"
+            value-format="YYYY-MM-DD"
           >
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="按条件查询" class="dateItem">
+          <el-select
+            class="FieldSelect"
+            v-model="FieldValue"
+            placeholder="Select"
+            size="large"
+            clearable
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+          <el-input v-model="anyField" placeholder="搜索" class="anyField" />
+        </el-form-item>
+
+        <el-form-item class="dateItem btn-w">
+          <div class="btn-df">
+            <el-button type="primary" @click="onQuireClick">查询</el-button>
+            <el-button type="primary" @click="onRestoreClick"
+              >取消查询</el-button
+            >
+            <el-button type="primary" @click="toggleSelection()"
+              >清除全选</el-button
+            >
+          </div>
+        </el-form-item>
+        <el-form-item class="dateItem">
+          <el-button type="primary" @click="AddNews">添加老师信息</el-button>
+          <el-button type="primary" @click="tableToExcel">导出</el-button>
         </el-form-item>
       </el-form>
     </div>
+    <!-- 表格信息展示 -->
     <div class="select-table">
       <el-table
         ref="multipleTableRef"
         row-class-name="table-heigth"
-        :data="tableData[studentListData.page - 1]"
+        :data="tableData[teacherListData.page - 1]"
         style="width: 100%"
         @selection-change="handleSelectionChange"
         highlight-current-row
         @current-change="handleCurrentChanges"
-        :default-sort="{ prop: 'price,address,name', order: 'descending' }"
+        :default-sort="{
+          prop: 'studentID,name,time,nationality,phone,sex,boarding,createdatetime,role',
+          order: 'descending',
+        }"
       >
         <el-table-column type="selection" width="55" />
-        <el-table-column sortable label="序号" width="180">
+        <el-table-column sortable prop="studentID" label="学号" width="150">
           <template #default="scope">
             <!-- <span v-show="!scope.row.isShow">{{ scope.row.price }}</span> -->
-            <span>{{ studentListData.id }}</span>
+            <span>{{ scope.row.studentID }}</span>
             <!-- <el-input
               v-show="scope.row.isShow"
-              v-model="studentListData.id"
+              v-model="teacherListData.id"
               placeholder="Please input"
             /> -->
           </template>
         </el-table-column>
-        <el-table-column sortable label="名字" width="180">
+        <el-table-column sortable prop="name" label="名字" width="150">
           <template #default="scope">
             <!-- <span v-show="!scope.row.isShow">{{ scope.row.name }}</span> -->
-            <span>{{ studentListData.name }}</span>
+            <span>{{ scope.row.name }}</span>
             <!-- <el-input
               v-show="scope.row.isShow"
-              v-model="studentListData.name"
+              v-model="teacherListData.name"
               placeholder="Please input"
             /> -->
           </template>
         </el-table-column>
-        <el-table-column sortable label="学号" width="180">
+        <el-table-column sortable prop="time" label="出生年月" width="150">
           <template #default="scope">
-            <!-- <span v-show="!scope.row.isShow">{{ scope.row.address }}</span> -->
-            <!-- <el-input
-              v-show="scope.row.isShow"
-              v-model="address[scope.$index].value"
-              placeholder="Please input"
-            /> -->
-            <span>{{ studentListData.studentID }}</span>
+            <span>{{ scope.row.time }}</span>
           </template>
         </el-table-column>
+        <el-table-column sortable prop="nationality" label="民族" width="150">
+          <template #default="scope">
+            <span>{{ scope.row.nationality }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column sortable prop="phone" label="手机号码" width="150">
+          <template #default="scope">
+            <span>{{ scope.row.phone }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column sortable prop="sex" label="性别" width="150">
+          <template #default="scope">
+            <span>{{ scope.row.sex }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column sortable prop="boarding" label="是否寄宿" width="150">
+          <template #default="scope">
+            <span>{{ scope.row.boarding }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          sortable
+          prop="createdatetime"
+          label="创建时间"
+          width="150"
+        >
+          <template #default="scope">
+            <span>{{ Times.reTime(scope.row.createdatetime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column sortable prop="role" label="身份" width="150">
+          <template #default="scope">
+            <span>{{ scope.row.role }}</span>
+          </template>
+        </el-table-column>
+
+        <!-- 按钮编辑 -->
         <el-table-column label="操作">
           <template #default="scope">
+            <!-- handleEdit(scope.$index, scope.row) -->
             <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
               >编辑</el-button
             >
+
             <el-button
               size="small"
               type="danger"
@@ -82,7 +147,7 @@
         </el-table-column>
       </el-table>
     </div>
-
+    <!-- 分页功能 -->
     <div class="example-pagination-block">
       <el-pagination
         :page-size="5"
@@ -90,51 +155,76 @@
         :default-current-page="1"
         :pager-count="7"
         layout="prev, pager, next"
-        :total="studentListData.count"
+        :total="teacherListData.count"
         background
         :hide-on-single-page="value"
         @current-change="handleCurrentChange"
       />
     </div>
   </div>
+  <!-- 个人信息修改弹窗 -->
+
+  <InformationPopup
+    :formDatas="formData"
+    ref="InformationPopup"
+    :dialogFormVisible="dialogFormVisible"
+  ></InformationPopup>
 </template>
 
 <script lang="ts">
-    import { defineComponent, onBeforeMount, reactive, Ref, ref, toRefs } from "vue";
+import {
+  defineComponent,
+  onBeforeMount,
+  reactive,
+  Ref,
+  ref,
+  toRefs,
+} from "vue";
 import { exRequest, RouterAPI } from "../../axios/api";
+import { TimeInit } from "../../TS/commons/time";
+import { InitData } from "../../TS/teacherList";
+import type { TeacherList } from "../../TS/teacherList";
+import InformationPopup from "../common_cpn/InformationPopup.vue";
 
-import { InitData } from "../../TS/studentList";
-
-    export default defineComponent({
-        name:'',
-        setup() {
-            const Data = reactive(new InitData());
-     function productData() {
+export default defineComponent({
+  name: "",
+  setup() {
+    const Data = reactive(new InitData());
+    function productData() {
       return exRequest.get({
         url: RouterAPI.Product,
         data: "",
       });
     }
+    const Times = new TimeInit();
     onBeforeMount(async () => {
       console.log("onBeforeMount");
       const routerData: Ref = ref();
       routerData.value = await productData();
       // .then((res) => {
-      Data.studentListData.count = routerData.value?.result.length;
+      Data.teacherListData.count = routerData.value?.result.length;
       Data.splitArray(routerData.value?.result);
-      //   return res.data.result;
-      // })
-      // .catch((err) => {
-      //   console.log(err);
-      // });
     });
-            return {
-                 ...toRefs(Data),
-            }
-        }
-    })
+    const InformationPopup = ref();
+    let handleEdit = (index: number, row: TeacherList) => {
+      let Datas = Data.tableData[Data.teacherListData.page - 1][index];
+      InformationPopup.value.changeBoon(Datas, false);
+    };
+    let AddNews = () => {
+      // let Datas = Data.tableData[Data.teacherListData.page - 1][index];
+      InformationPopup.value.changeBoon(Data.formData, true);
+    };
+    return {
+      ...toRefs(Data),
+      InformationPopup,
+      Times,
+      handleEdit,
+      AddNews,
+    };
+  },
+});
+
+// {"name":"张茵梅","sex":"0","phone":"15974278680","QQ":"1274512534","WX":"xgaw2","place":"校内教师公寓E201","IDcard":"442811198402114126",}
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
